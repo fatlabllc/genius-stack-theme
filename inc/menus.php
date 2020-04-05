4817c0bb4f128482861d9e8e232059e777b78968
S. Larrabee <slarrabee@fatlabllc.com>
2020-04-05 06:57:40 -0600

    modded editor roll access to appearance menu
    only Menu is now available.

--- inc/menus.php
+++ inc/menus.php
@@ -8,3 +8,24 @@ function wpb_custom_new_menu() {
 	//register_nav_menu('custom-menu',__( 'Custom Menu' ));
 }
 add_action( 'init', 'wpb_custom_new_menu' );
+
+// add editor the privilege to edit theme
+$role_object = get_role( 'editor' );
+$role_object->add_cap( 'edit_theme_options' );
+
+if (current_user_can('editor')) {
+	add_action( 'admin_menu', function () {
+	global $submenu;
+	if ( isset( $submenu[ 'themes.php' ] ) ) {
+	    foreach ( $submenu[ 'themes.php' ] as $index => $menu_item ) {
+	        foreach ($menu_item as $value) {
+	            if (strpos($value,'customize') !== false) {
+	                unset( $submenu[ 'themes.php' ][ $index ] );
+	            }
+	        }
+	    }
+	}
+	remove_submenu_page( 'themes.php', 'themes.php' );  
+	remove_submenu_page( 'themes.php', 'widgets.php' );
+	});
+}
