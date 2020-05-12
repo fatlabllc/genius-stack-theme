b35974843705a430b0110d32f5014d5ab7a72406
S. Larrabee <slarrabee@fatlabllc.com>
2020-05-12 17:17:19 -0600

    added transparent overlay option to slider
    light and dark options

--- template-parts/flexible-content/content-slider.php
+++ template-parts/flexible-content/content-slider.php
@@ -15,6 +15,7 @@
                     $show_text = get_sub_field('hero_display_text_over_image');
 	                $text_position = get_sub_field('hero_display_text_position');
                     $text  = get_sub_field( 'hero_text_overlay' );
+                    $overlay = get_sub_field('hero_transparentimage_overlay');
                     include( locate_template( 'template-parts/flexible-content/inc/carousel-slider.php', false, false ) );
                 endwhile;
             endif;
--- template-parts/flexible-content/inc/carousel-slider.php
+++ template-parts/flexible-content/inc/carousel-slider.php
@@ -6,7 +6,7 @@ if($default_header){
 	$alt_text = $image['alt'];
 }
 ?>
-<div class="carousel-item <?php if($count == 1) {echo 'active';}?>">
+<div class="carousel-item <?php if($count == 1) {echo 'active';} echo ' overlay-'.$overlay;?> ">
 	<img src="<?php echo $image['url']; ?>" alt="<?php echo $alt_text; ?>" class="img-fluid" data-no-lazy="1">
 	<div class="carousel-caption d-md-block align-middle">
 		<?php if($show_text){?>
