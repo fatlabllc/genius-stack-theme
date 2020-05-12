0ae444578ad645f2e17221464a9af3c621b6b992
S. Larrabee <slarrabee@fatlabllc.com>
2020-05-12 16:12:27 -0600

    remvoed cta btn controls
    in facor of new custom tinymce button / shorctcode generator

--- template-parts/flexible-content/inc/carousel-slider.php
+++ template-parts/flexible-content/inc/carousel-slider.php
@@ -9,30 +9,15 @@ if($default_header){
 <div class="carousel-item <?php if($count == 1) {echo 'active';}?>">
 	<img src="<?php echo $image['url']; ?>" alt="<?php echo $alt_text; ?>" class="img-fluid" data-no-lazy="1">
 	<div class="carousel-caption d-md-block align-middle">
-		<?php if($show_text){
-		    // check if there is a CTA
-            $cta = get_sub_field('show_cta_button');
-		    if($cta) {
-			    $button        = get_sub_field( 'hero_cta_button_link' );
-			    $button_url    = $button['url'];
-			    $button_title  = $button['title'];
-			    $button_target = $button['target'] ? $link['target'] : '_self';
-		    }
-		    ?>
+		<?php if($show_text){?>
             <div class="row">
                 <?php if($text_position == 'full'){?>
                     <div class="col-md-12">
-	                    <?php echo $text;
-	                    if($cta)
-	                        { include( locate_template( 'template-parts/flexible-content/inc/cta-button.php', false, false ) );
-	                    }?>
+	                    <?php echo $text;?>
                     </div>
                 <?php } elseif ($text_position == 'left') {?>
                     <div class="col-md-6">
-	                    <?php echo $text;
-	                    if($cta)
-	                    { include( locate_template( 'template-parts/flexible-content/inc/cta-button.php', false, false ) );
-	                    }?>
+	                    <?php echo $text;?>
                     </div>
                     <div class="col-md-6">
                     </div>
@@ -40,10 +25,7 @@ if($default_header){
                     <div class="col-md-6">
                     </div>
                     <div class="col-md-6">
-	                    <?php echo $text;
-	                    if($cta)
-	                    { include( locate_template( 'template-parts/flexible-content/inc/cta-button.php', false, false ) );
-	                    }?>
+	                    <?php echo $text;?>
                     </div>
                 <?php }?>
             </div>
--- template-parts/flexible-content/inc/cta-button.php
+++
@@ -1,3 +0,0 @@
-<div class="cta">
-	<a class="button" href="<?php echo esc_url( $button_url ); ?>" target="<?php echo esc_attr( $button_target ); ?>"><?php echo esc_html( $button_title ); ?></a>
-</div>
